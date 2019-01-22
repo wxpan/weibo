a995d0347a1a28ea55010bffff674175bab73c7e
wxp <pan1829@163.com>
2019-01-22 08:17:08 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,5 +28,11 @@ class SessionsController extends Controller
         }
     }
 
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
 
 }
