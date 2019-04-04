69633940903f0f330b0cf77b9ab6a93b130a4c70
wangxingpan <wangxingpan@gome.com.cn>
2019-04-04 18:26:26 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,4 +35,32 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit(User $user)
+    {
+//        compact('user')  得到 [user => $user] 的数组， $user 自动注入进来的 User 模型的实例
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $this->validate($request, [
+            //bail:在第一个验证规则失败时，就停止该字段接下来的规则验证
+            'name'     => 'bail|required|max:50',
+            //nullable:当用户提供空白密码时也会通过验证
+            'password' => 'bail|nullable|confirmed|min:6'
+
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+    }
 }
